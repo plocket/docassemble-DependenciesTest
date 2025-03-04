5a06b0851134a2ff35791b3579aa30981aefabe5
plocket <52798256+plocket@users.noreply.github.com>
2025-03-04 17:28:49 -0500

    Add dependencies

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.DependenciesTest',
       license='The MIT License (MIT)',
       url='https://docassemble.org',
       packages=find_namespace_packages(),
-      install_requires=[],
+      install_requires=['docassemble.AssemblyLine>=3.2.0', 'docassemble-AKA2JBranding @ https://github.com/A2JatAKCourts/docassemble-AKA2JBranding/archive/main.zip']
       zip_safe=False,
       package_data=find_package_data(where='docassemble/DependenciesTest/', package='docassemble.DependenciesTest'),
      )
